d3b00a56ab281dca5239e3c86ab7c3b43bfddf2b
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-27 18:29:15 +0000

    Working Solution

--- lib/change_converter.rb
+++ lib/change_converter.rb
@@ -1,12 +1,29 @@
 class ChangeConverter
   def convert(float)
-    if float == 0
-      []
-    elsif float == 0.01
-      ["1p"]
-    elsif float == 0.02
-      ["2p"]
-    end
-  end
+    change_hash = {
+      2000 => "£20",
+      1000 => "£10",
+      500 => "£5",
+      100 => "£1",
+      50 => "50p",
+      20 => "20p",
+      10 => "10p",
+      5 => "5p",
+      2 => "2p",
+      1 => "1p"
+    }
 
+    float *= 100
+
+    change_array = []
+
+    change_hash.each do |key, value|
+      while float >= key
+        change_array.push(value)
+        float -= key
+      end
+    end
+
+    return change_array
+  end
 end
--- spec/change_converter_spec.rb
+++ spec/change_converter_spec.rb
@@ -2,9 +2,6 @@ require 'change_converter'
 
 describe '#convert' do
   change_converter = ChangeConverter.new
-  it 'should accept 0.00 and return []' do
-    expect(change_converter.convert(0.00)).to eq []
-  end
 
   it 'should accept 0.01 and return ["1p"]' do
     expect(change_converter.convert(0.01)).to eq ["1p"]
@@ -12,5 +9,33 @@ describe '#convert' do
 
   it 'should accept 0.02 and return ["2p"]' do
     expect(change_converter.convert(0.02)).to eq ["2p"]
+  end
+
+  it 'should accept 0.05 and return ["5p"]' do
+    expect(change_converter.convert(0.05)).to eq ["5p"]
+  end
+
+  it 'should accept 0.03 and return ["2p", "1p"]' do
+    expect(change_converter.convert(0.03)).to eq ["2p", "1p"]
+  end
+
+  it 'should accept 0.08 and return ["5p","2p", "1p"]' do
+    expect(change_converter.convert(0.08)).to eq ["5p","2p", "1p"]
+  end
+
+  it 'should accept 0.04 and return ["2p","2p"]' do
+    expect(change_converter.convert(0.04)).to eq ["2p","2p"]
+  end
+
+  it 'should accept 0.44 and return ["20p", "20p","2p","2p"]' do
+    expect(change_converter.convert(0.44)).to eq ["20p", "20p","2p","2p"]
+  end
+
+  it 'should accept 0.44 and return["£5", "£1", "£1", "20p", "20p", "2p"]' do
+    expect(change_converter.convert(7.42)).to eq ["£5", "£1", "£1", "20p", "20p", "2p"]
+  end
+
+  it 'should accept 40 and return["£20", "£20"]' do
+    expect(change_converter.convert(40)).to eq ["£20", "£20"]
   end
 end
